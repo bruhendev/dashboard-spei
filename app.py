2fde28ff9a37b9e1ad80feaf4d8da869c053ee37
Bruno Henrique <brunohenrique242@gmail.com>
2024-08-01 15:01:09 -0300

    Teste de leitura dos dados

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 file_path = 'dados/ETP_HARVREAVES_TERRACLIMATE.xlsx'
-
 df = pd.read_excel(file_path)
+df = df.rename(columns={'Hargreaves Potential Evapotranspiration (TerraClimate)':'date', 'Unnamed: 1': 'value'})
 
+df = df.iloc[1:].reset_index(drop=True)
 df
