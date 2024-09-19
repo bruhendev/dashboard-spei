28a90d652bd0705afc2d1c0ca35c39518705692f
unknown <brunohenrique242@gmail.com>
2024-09-19 09:01:01 -0300

    Adicionando o dropdown e ajustando para funcionar corretamente

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import dash
 from dash import dcc, html
 import plotly.graph_objs as go
+from dash.dependencies import Input, Output
 
 # Função para extrair dados
 def extrair_dados(path_etp, path_prp, acumulado=1):
@@ -36,9 +37,16 @@ def extrair_dados(path_etp, path_prp, acumulado=1):
 file_path_etp = 'dados/ETP_HARVREAVES_TERRACLIMATE.xlsx'
 file_path_prp = 'dados/PRP_TERRACLIMATE.xlsx'
 
-# Extração dos dados e cálculo do SPEI para diferentes períodos de acumulação
+# Extração dos dados e cálculo do SPEI
 dados_1 = extrair_dados(file_path_etp, file_path_prp, 1)
 spei_1 = si.spei(pd.Series(dados_1['dados']))
+
+# Função para filtrar os anos
+def filtrarPorAno(spei, ano_inicial, ano_final):
+    if not isinstance(spei.index, pd.DatetimeIndex):
+        raise ValueError("O índice de 'spei' deve ser do tipo DatetimeIndex.")
+    
+    return spei[(spei.index.year >= ano_inicial) & (spei.index.year <= ano_final)]
 
 # Inicialização do aplicativo Dash
 app = dash.Dash(__name__)
@@ -47,25 +55,43 @@ app = dash.Dash(__name__)
 app.layout = html.Div(children=[
     html.H1(children='Dashboard de SPEI'),
 
-    dcc.Graph(
-        id='spei-1-graph',
-        figure={
-            'data': [
-                go.Scatter(
-                    x=spei_1.index,
-                    y=spei_1,
-                    mode='lines',
-                    name='SPEI-1'
-                )
-            ],
-            'layout': go.Layout(
-                title='SPEI-1 ao Longo do Tempo',
-                xaxis={'title': 'Data'},
-                yaxis={'title': 'SPEI'},
-            )
-        }
+    dcc.Dropdown(
+        id='ano-dropdown',
+        options=[
+            {'label': f'{ano} - {ano + 9}', 'value': f'{ano}-{ano + 9}'} for ano in range(1981, 2023, 10)
+        ],
+        value='1981-1990',  # Valor padrão
+        clearable=False
     ),
+
+    dcc.Graph(id='spei-1-graph')
 ])
+
+# Callback para atualizar o gráfico
+@app.callback(
+    Output('spei-1-graph', 'figure'),
+    Input('ano-dropdown', 'value')
+)
+def atualizar_grafico(intervalo):
+    ano_inicial, ano_final = map(int, intervalo.split('-'))
+    spei_filtrado = filtrarPorAno(spei_1, ano_inicial, ano_final)
+    
+    return {
+        'data': [
+            go.Scatter(
+                x=spei_filtrado.index,
+                y=spei_filtrado.values,
+                mode='lines',
+                name=f'SPEI de {ano_inicial} a {ano_final}'
+            )
+        ],
+        'layout': go.Layout(
+            title=f'SPEI de {ano_inicial} a {ano_final}',
+            xaxis={'title': 'Data'},
+            yaxis={'title': 'SPEI'},
+            hovermode='closest'
+        )
+    }
 
 # Executa o servidor
 if __name__ == '__main__':
